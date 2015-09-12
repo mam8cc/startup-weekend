5aaa979cc3e381614622bfda89c2650d644b5860
Mike <mapp86@mail.missouri.edu>
2015-09-12 11:58:25 -0500

    partial model updates

--- models/models.py
+++ models/models.py
@@ -12,10 +12,9 @@ class Backpack(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    weight = db.Column(db.Integer)
-    volume = db.Column(db.Integer)
     price = db.Column(db.Float)
     url = db.Column(db.String)
+    url_img = db.Column(db.String)
 
     # color_id = db.Column(db.ForeignKey())
     # gender_id = db.Column(db.ForeignKey())
@@ -28,29 +27,34 @@ class Backpack(db.Model):
         'weight': fields.Integer,
         'volume': fields.Integer,
         'price': fields.Float,
-        'url': fields.String
+        'url': fields.String,
+        'url_img': fields.String
     }
 
 
-class WaistDimension(db.Model):
-    __tablename__ = 'dimension_waist'
+class BackpackSize(db.Model):
+    __tablename__ = 'backpack_size'
 
     id = db.Column(db.Integer, primary_key=True)
     size_name = db.Column(db.String)
-    size_range_low = db.Column(db.Integer)
-    size_range_high = db.Column(db.Integer)
+    torso_range_low = db.Column(db.Float)
+    torso_range_high = db.Column(db.Float)
+    waist_range_low = db.Column(db.Float)
+    waist_range_high = db.Column(db.Float)
+    weight_oz = db.Column(db.Float)
+    volumn_liter = db.Column(db.Integer)
 
     backpack_id = db.Column(db.ForeignKey('backpack.id'))
 
     backpack = relationship('Backpack', lazy='joined')
 
-
-class TorsoDimension(db.Model):
-    __tablename__ = 'dimension_torso'
+class Gear(db.Model):
+    __tablename__ = 'gear'
 
     id = db.Column(db.Integer, primary_key=True)
-    size_name = db.Column(db.String)
-    size_range_low = db.Column(db.Integer)
-    size_range_high = db.Column(db.Integer)
+    name = db.Column(db.String)
+    weight = db.Column(db.Float)
+    length = db.Column(db.Float)
+    width = db.Column(db.Float)
+    height = db.Column(db.Float)
 
-    backpack_id = db.Column(db.ForeignKey('backpack.id'))
